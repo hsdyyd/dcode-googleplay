fc9645c156fddf6b6ae99b50aa580a59838991d0
hsdyyd <54978101@qq.com>
2016-09-12 15:04:42 +0800

    1.activity使用actionbar 2.导入相关图片资源文件

--- src/com/droid/googleplay/MainActivity.java
+++ src/com/droid/googleplay/MainActivity.java
@@ -1,23 +1,39 @@
 package com.droid.googleplay;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+/**
+ * @author dongyi
+ * 使用ActionBar时Activity继承自ActionBarActivity,需要引入v7包
+ * 修改主题:@style/Theme.AppComp.xxx
+ */
 public class MainActivity extends ActionBarActivity
 {
+	private ActionBar mActionBar;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		mActionBar = getSupportActionBar();
+		mActionBar.setTitle("GooglePlay");
+//		mActionBar.setSubtitle("good app");
+		mActionBar.setIcon(R.drawable.ic_launcher);
+		mActionBar.setDisplayShowTitleEnabled(true);
+		mActionBar.setDisplayShowHomeEnabled(true);
+		mActionBar.setDisplayHomeAsUpEnabled(true);
 	}
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu)
 	{
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
@@ -25,9 +41,6 @@ public class MainActivity extends ActionBarActivity
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item)
 	{
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 		if (id == R.id.action_settings)
 		{
